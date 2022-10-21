feb0a1e78c2f8dd78143f51ca6bb8af0e17e2089
Frisel91 <goryach.1991@mail.ru>
2022-10-21 19:29:41 +0300

    макет

---
+++ app/src/main/java/com/example/maketa/MainActivity.kt
@@ -0,0 +1,56 @@
+package com.example.maketa
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
+import androidx.appcompat.app.AlertDialog
+import androidx.navigation.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.setupActionBarWithNavController
+import androidx.navigation.ui.setupWithNavController
+import com.google.android.material.bottomnavigation.BottomNavigationView
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val navView: BottomNavigationView = findViewById(R.id.nav_view)
+
+        val navController = findNavController(R.id.nav_host_fragment)
+
+        val appBarConfiguration = AppBarConfiguration(setOf(
+            R.id.obrasenie,
+            R.id.profil
+        ))
+        setupActionBarWithNavController(navController, appBarConfiguration)
+        navView.setupWithNavController(navController)
+
+
+        var firstName ="Петров"
+        var compani ="compani"
+        var nameS = findViewById<TextView>(R.id.title_name)
+        nameS.text = firstName
+        val nameImg = findViewById<TextView>(R.id.textView3)
+        nameImg.text = firstName[0].toString()
+        val companiName = findViewById<TextView>(R.id.textView2)
+        companiName.text =compani
+
+
+        val buttonAlert = findViewById<Button>(R.id.button)
+
+        buttonAlert.setOnClickListener{
+            val builder = AlertDialog.Builder(this)
+            builder.setMessage("Вы действительно хотите выйти?")
+            builder.setPositiveButton("Да выйти") { dialog, _ ->dialog.dismiss()
+
+            }
+            builder.setNegativeButton("Отмена"){ dialog, _ -> dialog.dismiss()
+            }
+            // Create the AlertDialog
+            val alertDialog: AlertDialog = builder.create()
+            alertDialog.show()
+        }
+    }
+}
