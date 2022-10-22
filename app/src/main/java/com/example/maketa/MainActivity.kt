0b3ce5491545215cdd237ba1da8782ca21d30b82
Frisel91 <goryach.1991@mail.ru>
2022-10-22 17:00:30 +0300

    переписал переменные и логику кнопки во фрагмент

--- app/src/main/java/com/example/maketa/MainActivity.kt
+++ app/src/main/java/com/example/maketa/MainActivity.kt
@@ -5,8 +5,10 @@ import android.os.Bundle
 import android.widget.Button
 import android.widget.TextView
 import androidx.appcompat.app.AlertDialog
+import androidx.navigation.NavController
 import androidx.navigation.findNavController
 import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.NavigationUI
 import androidx.navigation.ui.setupActionBarWithNavController
 import androidx.navigation.ui.setupWithNavController
 import com.google.android.material.bottomnavigation.BottomNavigationView
@@ -17,40 +19,17 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         val navView: BottomNavigationView = findViewById(R.id.nav_view)
-
         val navController = findNavController(R.id.nav_host_fragment)
 
         val appBarConfiguration = AppBarConfiguration(setOf(
             R.id.obrasenie,
             R.id.profil
         ))
+
         setupActionBarWithNavController(navController, appBarConfiguration)
         navView.setupWithNavController(navController)
 
 
-        val firstName ="Петров"
-        val compani ="compani"
-        val nameS = findViewById<TextView>(R.id.title_name)
-        nameS.text = firstName
-        val nameImg = findViewById<TextView>(R.id.textView3)
-        nameImg.text = firstName[0].toString()
-        val companiName = findViewById<TextView>(R.id.textView2)
-        companiName.text =compani
-
-
-        val buttonAlert = findViewById<Button>(R.id.button)
-
-        buttonAlert.setOnClickListener{
-            val builder = AlertDialog.Builder(this)
-            builder.setMessage("Вы действительно хотите выйти?")
-            builder.setPositiveButton("Да выйти") { dialog, _ ->dialog.dismiss()
-
-            }
-            builder.setNegativeButton("Отмена"){ dialog, _ -> dialog.dismiss()
-            }
-            // Create the AlertDialog
-            val alertDialog: AlertDialog = builder.create()
-            alertDialog.show()
-        }
     }
 }
+
---
+++ app/src/main/java/com/example/maketa/Profile.kt
@@ -0,0 +1,78 @@
+package com.example.maketa
+
+import android.app.Activity
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Button
+import android.widget.TextView
+import androidx.appcompat.app.AlertDialog
+import androidx.navigation.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.NavigationUI
+import androidx.navigation.ui.NavigationUI.setupActionBarWithNavController
+import androidx.navigation.ui.setupActionBarWithNavController
+import androidx.navigation.ui.setupWithNavController
+import com.google.android.material.bottomnavigation.BottomNavigationView
+
+
+class Profile : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ):
+            View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_profil, container, false);
+
+
+    }
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        val firstName ="Петров"
+        val compani ="compani"
+
+        val nameS = view.findViewById<TextView>(R.id.title_name)
+        val nameImg = view.findViewById<TextView>(R.id.textView3)
+        val companiName = view.findViewById<TextView>(R.id.textView2)
+
+        nameImg.text = firstName[0].toString()
+        companiName.text =compani
+        nameS.text = firstName
+
+
+        val buttonAlert = view.findViewById<Button>(R.id.button)
+        buttonAlert.setOnClickListener{
+            val builder = AlertDialog.Builder(view.context)
+                .setMessage("Вы действительно хотите выйти?")
+                .setPositiveButton("Да выйти", null)
+                .setNegativeButton("Отмена", null)
+
+                // Create the AlertDialog
+                .create()
+
+            builder.setOnShowListener {
+                builder.getButton(AlertDialog.BUTTON_NEGATIVE).setOnClickListener {
+                    builder.cancel()
+                }
+            }
+            builder.setOnShowListener {
+                builder.getButton(AlertDialog.BUTTON_POSITIVE).setOnClickListener {
+                    builder.cancel()
+
+                  //  navView.selectedItemId = R.id.obrasenie
+                }
+            }
+
+            builder.show()
+        }
+        }
+    }
+
+
+
+
